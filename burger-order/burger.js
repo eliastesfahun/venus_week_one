3d310afe7fef2ea6f713f1d8782a205e5e8134c6
elias <eltes2010@gmail.com>
2025-03-20 19:47:24 +0100

    advanced the js

--- burger-order/burger.js
+++ burger-order/burger.js
@@ -1,1 +1,15 @@
 console.log("Welcome to the Burger Order System!");
+
+const nameOfTheBurger="combo meal"
+const priceOfTheBurger=3
+const quantity=2
+
+var totalPrice =quantity * priceOfTheBurger
+
+if(nameOfTheBurger==="combo meal"){
+   
+    totalPrice *= 1.5
+    
+}
+console.log(`the name of the burger is: ${nameOfTheBurger} and the price is : ${priceOfTheBurger} `) 
+console.log(`the name of the burger is ${nameOfTheBurger} and the total price for ${quantity} burgers is ${totalPrice} `)
